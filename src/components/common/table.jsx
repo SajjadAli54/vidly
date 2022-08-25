29dfe2267e8c0c7e24df4416e2cfeb9e550bf560
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-08-25 14:56:06 +0500

    Added props destructuring

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
-const Table = (props) => {
-  const { data, columns, sortColumn, onSort } = props;
+const Table = ({ data, columns, sortColumn, onSort }) => {
   return (
     <table className="table table-hover table-responsive">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
